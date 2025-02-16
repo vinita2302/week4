fcae8ff34aab49dd877674d6c86e49284c02c004
vinita patanker <vinitapatanker@gmail.com>
2025-02-16 15:43:48 +0530

    [vinita patanke] Day05 files

--- Day_05_Regex/src/main/java/junit_testing/basic_unit_testing/testing_parameterized_tests/NumberUtils.java
+++
@@ -1,9 +0,0 @@
-package junit_testing.basic_unit_testing.testing_parameterized_tests;
-
-public class NumberUtils {
-
-    // Method to check if a number is even
-    public boolean isEven(int number) {
-        return number % 2 == 0;
-    }
-}
